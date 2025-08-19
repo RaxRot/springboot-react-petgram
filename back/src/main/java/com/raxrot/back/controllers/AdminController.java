2935a32f6a9c2c9dc850670a22198eeac715bc63
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-08-19 17:56:08 +0100

    add crud operations

--- back/src/main/java/com/raxrot/back/controllers/AdminController.java
+++ back/src/main/java/com/raxrot/back/controllers/AdminController.java
@@ -1,18 +1,50 @@
 package com.raxrot.back.controllers;
 
+import com.raxrot.back.configs.AppConstants;
+import com.raxrot.back.dtos.UserPageResponse;
+import com.raxrot.back.dtos.UserResponse;
 import com.raxrot.back.services.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/admin/users")
 @RequiredArgsConstructor
 public class AdminController {
     private final UserService userService;
+
+    @GetMapping
+    public ResponseEntity<UserPageResponse> getAllUsers(
+            @RequestParam(name = "pageNumber", defaultValue = AppConstants.PAGE_NUMBER, required = false) Integer pageNumber,
+            @RequestParam(name = "pageSize", defaultValue = AppConstants.PAGE_SIZE, required = false) Integer pageSize,
+            @RequestParam(name = "sortBy", defaultValue = AppConstants.SORT_USERS_BY, required = false) String sortBy,
+            @RequestParam(name = "sortOrder", defaultValue = AppConstants.SORT_DIR, required = false) String sortOrder) {
+        UserPageResponse userPageResponse = userService.getAllUsers(pageNumber, pageSize, sortBy, sortOrder);
+        return new ResponseEntity<>(userPageResponse, HttpStatus.OK);
+    }
+    @GetMapping("/id/{id}")
+    public ResponseEntity<UserResponse> getUserById(@PathVariable Long id) {
+        UserResponse userResponse = userService.getUserById(id);
+        return new ResponseEntity<>(userResponse, HttpStatus.OK);
+    }
+    @GetMapping("/email/{email}")
+    public ResponseEntity<UserResponse> getUserByEmail(@PathVariable String email) {
+        UserResponse userResponse = userService.getUserByEmail(email);
+        return new ResponseEntity<>(userResponse, HttpStatus.OK);
+    }
+
+    @PatchMapping("/ban/{id}")
+    public ResponseEntity<Void>banUser(@PathVariable Long id) {
+        userService.banUser(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+    @PatchMapping("/unban/{id}")
+    public ResponseEntity<Void>unBanUser(@PathVariable Long id) {
+        userService.unbanUser(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
