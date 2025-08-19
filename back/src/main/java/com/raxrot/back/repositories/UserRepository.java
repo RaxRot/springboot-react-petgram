0d452b84eaf725125c4d26cbaeb911032be154e7
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-08-19 13:17:28 +0100

    add UserRepository

---
+++ back/src/main/java/com/raxrot/back/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package com.raxrot.back.repositories;
+
+import com.raxrot.back.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUserName(String userName);
+    boolean existsByEmail(String email);
+    boolean existsByUserName(String userName);
+}
