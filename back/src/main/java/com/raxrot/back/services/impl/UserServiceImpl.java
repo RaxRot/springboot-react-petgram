14c13ebebd4530d165787c59adefacfddc851e1c
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-08-19 17:29:42 +0100

    add crud and ban/unban impl

--- back/src/main/java/com/raxrot/back/services/impl/UserServiceImpl.java
+++ back/src/main/java/com/raxrot/back/services/impl/UserServiceImpl.java
@@ -1,6 +1,8 @@
 package com.raxrot.back.services.impl;
 
+import com.raxrot.back.dtos.UserPageResponse;
 import com.raxrot.back.dtos.UserResponse;
+import com.raxrot.back.dtos.UserResponseForSearch;
 import com.raxrot.back.exceptions.ApiException;
 import com.raxrot.back.models.AppRole;
 import com.raxrot.back.models.User;
@@ -11,10 +13,17 @@ import com.raxrot.back.services.UserService;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -23,6 +32,7 @@ public class UserServiceImpl implements UserService {
     private final ModelMapper modelMapper;
     private final FileUploadService fileUploadService;
 
+    @Transactional
     @Override
     public UserResponse uploadImgProfilePic(MultipartFile file, Authentication authentication) {
         if (file == null || file.isEmpty()) {
@@ -69,5 +79,68 @@ public class UserServiceImpl implements UserService {
         }else{
             throw new ApiException("Impossible to delete ADMIN", HttpStatus.CONFLICT);
         }
+    }
+
+    @Override
+    public UserPageResponse getAllUsers(Integer pageNumber, Integer pageSize, String sortBy, String sortOrder) {
+        Sort sort = sortOrder.equalsIgnoreCase("desc") ?
+                Sort.by(sortBy).descending() :
+                Sort.by(sortBy).ascending();
+
+        Pageable pageDetails = PageRequest.of(pageNumber, pageSize, sort);
+        Page<User> userPage = userRepository.findAll(pageDetails);
+
+
+        List<UserResponse> userResponses = userPage.getContent().stream()
+                .map(user -> modelMapper.map(user, UserResponse.class))
+                .collect(Collectors.toList());
+
+        UserPageResponse userPageResponse = new UserPageResponse();
+        userPageResponse.setContent(userResponses);
+        userPageResponse.setPageNumber(userPage.getNumber());
+        userPageResponse.setPageSize(userPage.getSize());
+        userPageResponse.setTotalElements(userPage.getTotalElements());
+        userPageResponse.setTotalPages(userPage.getTotalPages());
+        userPageResponse.setLastPage(userPage.isLast());
+        return userPageResponse;
+    }
+
+    @Override
+    public UserResponse getUserById(Long userId) {
+        User user=userRepository.findById(userId)
+                .orElseThrow(() -> new ApiException("User not found", HttpStatus.NOT_FOUND));
+        return modelMapper.map(user, UserResponse.class);
+    }
+
+    @Override
+    public UserResponse getUserByEmail(String email) {
+        User user=userRepository.findByEmail(email)
+                .orElseThrow(() -> new ApiException("User not found", HttpStatus.NOT_FOUND));
+        return modelMapper.map(user, UserResponse.class);
+    }
+
+    @Override
+    public UserResponseForSearch getUserByUsername(String username) {
+        User user=userRepository.findByUserName(username)
+                .orElseThrow(() -> new ApiException("User not found", HttpStatus.NOT_FOUND));
+        return modelMapper.map(user, UserResponseForSearch.class);
+    }
+
+    @Transactional
+    @Override
+    public void banUser(Long userId) {
+        User user=userRepository.findById(userId)
+                .orElseThrow(() -> new ApiException("User not found", HttpStatus.NOT_FOUND));
+        user.setBanned(true);
+        userRepository.save(user);
+    }
+
+    @Transactional
+    @Override
+    public void unbanUser(Long userId) {
+        User user=userRepository.findById(userId)
+                .orElseThrow(() -> new ApiException("User not found", HttpStatus.NOT_FOUND));
+        user.setBanned(false);
+        userRepository.save(user);
     }
 }
