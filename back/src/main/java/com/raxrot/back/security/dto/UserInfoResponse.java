13aa58400ca560871ceec00e29e897667e25eae6
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-08-19 13:25:38 +0100

    add UserInfoResponse

---
+++ back/src/main/java/com/raxrot/back/security/dto/UserInfoResponse.java
@@ -0,0 +1,32 @@
+package com.raxrot.back.security.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class UserInfoResponse {
+    private Long id;
+    private String jwtToken;
+    private String username;
+    private String email;
+    private List<String> roles;
+
+    public UserInfoResponse(Long id, String username, List<String> roles) {
+        this.id = id;
+        this.username = username;
+        this.roles = roles;
+    }
+    public UserInfoResponse(Long id, String username,String email, List<String> roles) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.roles = roles;
+    }
+}
