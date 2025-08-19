fe90fd2bd791dc36fd12d8787e749931a07a461f
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-08-19 17:29:35 +0100

    add crud and ban/unban

--- back/src/main/java/com/raxrot/back/services/UserService.java
+++ back/src/main/java/com/raxrot/back/services/UserService.java
@@ -1,10 +1,18 @@
 package com.raxrot.back.services;
 
+import com.raxrot.back.dtos.UserPageResponse;
 import com.raxrot.back.dtos.UserResponse;
+import com.raxrot.back.dtos.UserResponseForSearch;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface UserService {
     UserResponse uploadImgProfilePic(MultipartFile file, Authentication authentication);
     void deleteUserById(Long userId);
+    UserPageResponse getAllUsers(Integer pageNumber, Integer pageSize, String sortBy, String sortOrder);
+    UserResponse getUserById(Long userId);
+    UserResponse getUserByEmail(String email);
+    UserResponseForSearch getUserByUsername(String username);
+    void banUser(Long userId);
+    void unbanUser(Long userId);
 }
