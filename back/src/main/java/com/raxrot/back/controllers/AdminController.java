57d9a0ae573787b8e5a822ec6c09afd944d2a565
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-08-19 15:08:50 +0100

    add AdminController with delete user

---
+++ back/src/main/java/com/raxrot/back/controllers/AdminController.java
@@ -0,0 +1,22 @@
+package com.raxrot.back.controllers;
+
+import com.raxrot.back.services.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/admin/users")
+@RequiredArgsConstructor
+public class AdminController {
+    private final UserService userService;
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+        userService.deleteUserById(id);
+        return ResponseEntity.noContent().build();
+    }
+}
