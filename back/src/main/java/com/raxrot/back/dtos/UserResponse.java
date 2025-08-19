ffba844c32cd87ead37f80cfb570ec1be8259307
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-08-19 14:17:30 +0100

    add UserResponse

---
+++ back/src/main/java/com/raxrot/back/dtos/UserResponse.java
@@ -0,0 +1,17 @@
+package com.raxrot.back.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserResponse {
+    private Long id;
+    private String userName;
+    private String email;
+    private String profilePic;
+}
