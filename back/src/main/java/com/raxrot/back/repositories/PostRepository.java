0e547cc2e73f460aff5473dd32d6c57be54edd3c
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-08-20 12:10:34 +0100

    add PostRepository

---
+++ back/src/main/java/com/raxrot/back/repositories/PostRepository.java
@@ -0,0 +1,12 @@
+package com.raxrot.back.repositories;
+
+import com.raxrot.back.enums.AnimalType;
+import com.raxrot.back.models.Post;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
+    Page<Post> findAllByUser_UserName(String username, Pageable pageable);
+    Page<Post> findAllByAnimalType(AnimalType animalType, Pageable pageable);
+}
