39b4171694749651211681195a574d8e4f734fa6
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-08-20 14:14:08 +0100

    add LikeRepository

---
+++ back/src/main/java/com/raxrot/back/repositories/LikeRepository.java
@@ -0,0 +1,10 @@
+package com.raxrot.back.repositories;
+
+import com.raxrot.back.models.Like;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LikeRepository  extends JpaRepository<Like, Long> {
+    boolean existsByPost_IdAndUser_UserId(Long postId, Long userId);
+    long countByPost_Id(Long postId);
+    void deleteByPost_IdAndUser_UserId(Long postId, Long userId);
+}
