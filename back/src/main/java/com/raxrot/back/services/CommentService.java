42de7779c2bc229480727ac8317ca1e4be6511aa
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-08-20 16:10:52 +0100

    add CommentService

---
+++ back/src/main/java/com/raxrot/back/services/CommentService.java
@@ -0,0 +1,12 @@
+package com.raxrot.back.services;
+
+import com.raxrot.back.dtos.CommentPageResponse;
+import com.raxrot.back.dtos.CommentRequest;
+import com.raxrot.back.dtos.CommentResponse;
+
+public interface CommentService {
+    CommentResponse addComment(Long postId, CommentRequest req);
+    CommentPageResponse getComments(Long postId, Integer page, Integer size, String sortBy, String sortOrder);
+    CommentResponse updateComment(Long commentId, CommentRequest req);
+    void deleteComment(Long commentId);
+}
