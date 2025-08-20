098004aace13f90ad159bad0b0a2808eae3c9cc0
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-08-20 12:57:27 +0100

    improve code(remoce Authentication as we have AuthUtil)

--- back/src/main/java/com/raxrot/back/services/PostService.java
+++ back/src/main/java/com/raxrot/back/services/PostService.java
@@ -3,13 +3,12 @@ package com.raxrot.back.services;
 import com.raxrot.back.dtos.PostPageResponse;
 import com.raxrot.back.dtos.PostRequest;
 import com.raxrot.back.dtos.PostResponse;
-import org.springframework.security.core.Authentication;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface PostService {
-    PostResponse createPost(MultipartFile file, PostRequest postRequest, Authentication authentication);
+    PostResponse createPost(MultipartFile file, PostRequest postRequest);
     PostPageResponse getAllPosts(Integer pageNumber, Integer pageSize, String sortBy, String sortOrder);
     PostResponse getPostById(Long postId);
     PostPageResponse getPostsByUsername(String username, Integer pageNumber, Integer pageSize, String sortBy, String sortOrder);
-    void deletePost(Long postId, Authentication authentication);
+    void deletePost(Long postId);
 }
