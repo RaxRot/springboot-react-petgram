55f00c35cacb5c5264b56edb2c682e03ca80b679
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-08-20 12:20:04 +0100

    add PostRequest

---
+++ back/src/main/java/com/raxrot/back/dtos/PostRequest.java
@@ -0,0 +1,24 @@
+package com.raxrot.back.dtos;
+
+import com.raxrot.back.enums.AnimalType;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class PostRequest {
+    @NotBlank(message = "Title is required")
+    @Size(min = 3, max = 120, message = "Title must be between 3 and 120 characters")
+    private String title;
+
+    private String content;
+    private String imageUrl;
+
+    private AnimalType animalType = AnimalType.OTHER;
+}
