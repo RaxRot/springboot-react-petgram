aef01b7dc0c5b324a8d916d98db7841d16424269
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-08-21 16:04:54 +0100

    Add FollowController

---
+++ back/src/main/java/com/raxrot/back/controllers/FollowController.java
@@ -0,0 +1,37 @@
+package com.raxrot.back.controllers;
+
+import com.raxrot.back.services.FollowService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class FollowController {
+    private final FollowService followService;
+
+    @PostMapping("/users/{userId}/follow")
+    public ResponseEntity<Void> follow(@PathVariable Long userId) {
+        followService.followUser(userId);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/users/{userId}/follow")
+    public ResponseEntity<Void> unfollow(@PathVariable Long userId) {
+        followService.unfollowUser(userId);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/public/users/{userId}/followers/count")
+    public ResponseEntity<Long> getFollowersCount(@PathVariable Long userId) {
+        long followersCount = followService.getFollowersCount(userId);
+        return ResponseEntity.ok(followersCount);
+    }
+
+    @GetMapping("/public/users/{userId}/following/count")
+    public ResponseEntity<Long> getFollowingCount(@PathVariable Long userId) {
+        long followingCount = followService.getFollowingCount(userId);
+        return ResponseEntity.ok(followingCount);
+    }
+}
