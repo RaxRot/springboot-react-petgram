391098e430e7efd0ef3cf7e71766e5dbe9a54c03
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-08-21 16:04:30 +0100

    Add FollowRepository

---
+++ back/src/main/java/com/raxrot/back/repositories/FollowRepository.java
@@ -0,0 +1,12 @@
+package com.raxrot.back.repositories;
+
+import com.raxrot.back.models.Follow;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FollowRepository extends JpaRepository<Follow, Long> {
+    long countByFollowee_UserId(Long userId);
+    long countByFollower_UserId(Long userId);
+
+    boolean existsByFollower_UserIdAndFollowee_UserId(Long followerId, Long followeeId);
+    void deleteByFollower_UserIdAndFollowee_UserId(Long followerId, Long followeeId);
+}
