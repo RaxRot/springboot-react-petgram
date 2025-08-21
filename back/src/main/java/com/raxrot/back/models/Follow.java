47e0f0975e50a7f493ffeda4bdb2b9472ff807c0
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-08-21 16:04:20 +0100

    Add Folow

---
+++ back/src/main/java/com/raxrot/back/models/Follow.java
@@ -0,0 +1,43 @@
+package com.raxrot.back.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(
+        name = "tbl_follows",
+        uniqueConstraints = {
+                @UniqueConstraint(columnNames = {"follower_id", "followee_id"})
+        },
+        indexes = {
+                @Index(name = "idx_follower_id", columnList = "follower_id"),
+                @Index(name = "idx_followee_id", columnList = "followee_id")
+        }
+)
+public class Follow {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "follower_id", nullable = false)
+    private User follower;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "followee_id", nullable = false)
+    private User followee;
+
+    @Column(updatable = false)
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+}
