2e1dd7ba3df9a7112a700356418765fd6fcbd167
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-08-21 12:16:07 +0100

    add UpdateUsernameRequest

---
+++ back/src/main/java/com/raxrot/back/dtos/UpdateUsernameRequest.java
@@ -0,0 +1,18 @@
+package com.raxrot.back.dtos;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UpdateUsernameRequest {
+    @NotBlank(message = "Username cannot be blank")
+    @Size(min = 3, max = 20, message = "Username must be between 3 and 20 characters")
+    private String newUsername;
+}
