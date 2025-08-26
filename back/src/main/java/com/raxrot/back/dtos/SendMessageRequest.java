2f6dac06482b63d5d0faa68c15025b6f41d1b401
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-08-26 15:28:44 +0100

    add SendMessageRequest

---
+++ back/src/main/java/com/raxrot/back/dtos/SendMessageRequest.java
@@ -0,0 +1,18 @@
+package com.raxrot.back.dtos;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class SendMessageRequest {
+    @NotBlank
+    @Size(max = 2000)
+    private String text;
+}
