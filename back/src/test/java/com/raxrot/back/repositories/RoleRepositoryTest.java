fa0578a49e7dbe4a8ac4a7c752675bc1edc3329a
Dasha <m4daria@gmail.com>
2025-10-13 14:45:55 +0100

    test commit role repo

--- back/src/test/java/com/raxrot/back/repositories/RoleRepositoryTest.java
+++ back/src/test/java/com/raxrot/back/repositories/RoleRepositoryTest.java
@@ -11,7 +11,7 @@ import org.springframework.test.context.ActiveProfiles;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+//test from dari
 @DataJpaTest
 @ActiveProfiles("test")
 class RoleRepositoryTest {
