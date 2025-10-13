ba16eb8f8d20ea058ffcd5e40fdfbba05ec36af8
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-10-13 15:04:05 +0100

    fix test to work with CI

--- back/src/test/java/com/raxrot/back/BackApplicationTests.java
+++ back/src/test/java/com/raxrot/back/BackApplicationTests.java
@@ -1,9 +1,8 @@
 package com.raxrot.back;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class BackApplicationTests {
 
     @Test
