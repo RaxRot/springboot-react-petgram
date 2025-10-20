75529717d9809b7339b5d6ae4458d7f838c69e1e
Dasha <m4daria@gmail.com>
2025-10-20 15:58:00 +0100

    add logs to FileUploadServiceImpl.java

--- back/src/main/java/com/raxrot/back/services/impl/FileUploadServiceImpl.java
+++ back/src/main/java/com/raxrot/back/services/impl/FileUploadServiceImpl.java
@@ -2,6 +2,7 @@ package com.raxrot.back.services.impl;
 
 import com.raxrot.back.exceptions.ApiException;
 import com.raxrot.back.services.FileUploadService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -16,6 +17,7 @@ import software.amazon.awssdk.services.s3.model.PutObjectResponse;
 import java.io.IOException;
 import java.util.UUID;
 
+@Slf4j
 @Service
 public class FileUploadServiceImpl implements FileUploadService {
 
@@ -34,7 +36,10 @@ public class FileUploadServiceImpl implements FileUploadService {
     @Override
     public String uploadFile(MultipartFile file) {
         String originalFilename = file.getOriginalFilename();
+        log.info("Starting file upload: original filename='{}'", originalFilename);
+
         if (originalFilename == null || !originalFilename.contains(".")) {
+            log.warn("Invalid file name provided: '{}'", originalFilename);
             throw new ApiException("Invalid file name", HttpStatus.BAD_REQUEST);
         }
 
@@ -49,17 +54,24 @@ public class FileUploadServiceImpl implements FileUploadService {
                     .contentType(file.getContentType())
                     .build();
 
+            log.debug("Uploading file to S3: bucket='{}', key='{}', contentType='{}'", bucketName, key, file.getContentType());
+
             PutObjectResponse response = s3Client.putObject(
                     putObjectRequest,
                     RequestBody.fromBytes(file.getBytes())
             );
 
             if (response.sdkHttpResponse().isSuccessful()) {
-                return String.format("https://%s.s3.%s.amazonaws.com/%s", bucketName, region, key);
+                String url = String.format("https://%s.s3.%s.amazonaws.com/%s", bucketName, region, key);
+                log.info("File uploaded successfully to S3. URL={}", url);
+                return url;
             } else {
+                log.error("Failed to upload file to S3 (HTTP status: {})", response.sdkHttpResponse().statusCode());
                 throw new ApiException("Failed to upload file to S3", HttpStatus.BAD_REQUEST);
             }
+
         } catch (IOException e) {
+            log.error("Error reading file bytes for '{}': {}", originalFilename, e.getMessage(), e);
             throw new RuntimeException("Error reading file bytes", e);
         }
     }
@@ -67,10 +79,19 @@ public class FileUploadServiceImpl implements FileUploadService {
     @Override
     public void deleteFile(String imageUrl) {
         String fileName = imageUrl.substring(imageUrl.lastIndexOf("/") + 1);
+        log.info("Attempting to delete file '{}' from bucket '{}'", fileName, bucketName);
+
         DeleteObjectRequest deleteObjectRequest = DeleteObjectRequest.builder()
                 .bucket(bucketName)
                 .key(fileName)
                 .build();
-        s3Client.deleteObject(deleteObjectRequest);
+
+        try {
+            s3Client.deleteObject(deleteObjectRequest);
+            log.info("File '{}' successfully deleted from S3 bucket '{}'", fileName, bucketName);
+        } catch (Exception e) {
+            log.error("Failed to delete file '{}' from S3 bucket '{}': {}", fileName, bucketName, e.getMessage(), e);
+            throw new ApiException("Failed to delete file from S3", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
