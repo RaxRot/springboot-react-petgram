a5612a270981d9fab84242542b6329ed72f17b18
Dasha <m4daria@gmail.com>
2025-10-20 15:57:14 +0100

    add logs to CommentServiceImpl.java

--- back/src/main/java/com/raxrot/back/services/impl/CommentServiceImpl.java
+++ back/src/main/java/com/raxrot/back/services/impl/CommentServiceImpl.java
@@ -14,6 +14,7 @@ import com.raxrot.back.services.CommentService;
 import com.raxrot.back.utils.AuthUtil;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -26,40 +27,46 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class CommentServiceImpl implements CommentService {
+
     private final CommentRepository commentRepository;
     private final PostRepository postRepository;
     private final AuthUtil authUtil;
     private final ModelMapper modelMapper;
 
-
     @Transactional
     @Override
     public CommentResponse addComment(Long postId, CommentRequest req) {
-        User user=authUtil.loggedInUser();
-        if (user.isBanned()){
+        User user = authUtil.loggedInUser();
+        log.info("User '{}' attempting to add comment to post ID {}", user.getUserName(), postId);
+
+        if (user.isBanned()) {
+            log.warn("Banned user '{}' attempted to comment on post ID {}", user.getUserName(), postId);
             throw new ApiException("User is banned", HttpStatus.FORBIDDEN);
         }
-        Post post=postRepository.findById(postId)
-                .orElseThrow(()->new ApiException("Post not found", HttpStatus.NOT_FOUND));
 
-        Comment comment=modelMapper.map(req, Comment.class);
+        Post post = postRepository.findById(postId)
+                .orElseThrow(() -> {
+                    log.error("Post with ID {} not found while user '{}' tried to comment", postId, user.getUserName());
+                    return new ApiException("Post not found", HttpStatus.NOT_FOUND);
+                });
+
+        Comment comment = modelMapper.map(req, Comment.class);
         comment.setPost(post);
         comment.setAuthor(user);
-        Comment savedComment=commentRepository.save(comment);
+        Comment savedComment = commentRepository.save(comment);
 
-
-
+        log.info("User '{}' added comment (ID={}) to post ID {}", user.getUserName(), savedComment.getId(), postId);
         return modelMapper.map(savedComment, CommentResponse.class);
     }
 
     @Override
-    public CommentPageResponse getComments(Long postId,
-                                           Integer page,
-                                           Integer size,
-                                           String sortBy,
-                                           String sortOrder) {
+    public CommentPageResponse getComments(Long postId, Integer page, Integer size, String sortBy, String sortOrder) {
+        log.info("Fetching comments for post ID {} (page={}, size={}, sortBy={}, order={})", postId, page, size, sortBy, sortOrder);
+
         if (!postRepository.existsById(postId)) {
+            log.error("Attempt to fetch comments for non-existing post ID {}", postId);
             throw new ApiException("Post not found", HttpStatus.NOT_FOUND);
         }
 
@@ -81,43 +88,68 @@ public class CommentServiceImpl implements CommentService {
         resp.setTotalElements(commentPage.getTotalElements());
         resp.setTotalPages(commentPage.getTotalPages());
         resp.setLastPage(commentPage.isLast());
+
+        log.info("Fetched {} comments for post ID {}", commentPage.getTotalElements(), postId);
         return resp;
     }
 
     @Transactional
     @Override
     public CommentResponse updateComment(Long commentId, CommentRequest req) {
-        User user=authUtil.loggedInUser();
-        if (user.isBanned()){
+        User user = authUtil.loggedInUser();
+        log.info("User '{}' attempting to update comment ID {}", user.getUserName(), commentId);
+
+        if (user.isBanned()) {
+            log.warn("Banned user '{}' attempted to update comment ID {}", user.getUserName(), commentId);
             throw new ApiException("User is banned", HttpStatus.FORBIDDEN);
         }
-        Comment comment=commentRepository.findById(commentId)
-                .orElseThrow(()->new ApiException("Comment not found", HttpStatus.NOT_FOUND));
-        if (!comment.getAuthor().getUserId().equals(user.getUserId())){
+
+        Comment comment = commentRepository.findById(commentId)
+                .orElseThrow(() -> {
+                    log.error("Comment with ID {} not found for update", commentId);
+                    return new ApiException("Comment not found", HttpStatus.NOT_FOUND);
+                });
+
+        if (!comment.getAuthor().getUserId().equals(user.getUserId())) {
+            log.warn("User '{}' tried to update someone else's comment ID {}", user.getUserName(), commentId);
             throw new ApiException("You are not allowed to update this comment", HttpStatus.FORBIDDEN);
         }
+
         comment.setText(req.getText());
-        Comment updatedComment=commentRepository.save(comment);
+        Comment updatedComment = commentRepository.save(comment);
+
+        log.info("User '{}' updated comment ID {}", user.getUserName(), commentId);
         return modelMapper.map(updatedComment, CommentResponse.class);
     }
 
     @Transactional
     @Override
     public void deleteComment(Long commentId) {
-        User user=authUtil.loggedInUser();
-        Comment comment=commentRepository.findById(commentId)
-                .orElseThrow(()->new ApiException("Comment not found", HttpStatus.NOT_FOUND));
-        boolean isAdmin= user.getRoles().stream().anyMatch(r -> r.getRoleName() == AppRole.ROLE_ADMIN);
-        boolean isOwner=comment.getAuthor().getUserId().equals(user.getUserId());
-        if (isAdmin || isOwner){
+        User user = authUtil.loggedInUser();
+        log.info("User '{}' attempting to delete comment ID {}", user.getUserName(), commentId);
+
+        Comment comment = commentRepository.findById(commentId)
+                .orElseThrow(() -> {
+                    log.error("Comment with ID {} not found for deletion", commentId);
+                    return new ApiException("Comment not found", HttpStatus.NOT_FOUND);
+                });
+
+        boolean isAdmin = user.getRoles().stream().anyMatch(r -> r.getRoleName() == AppRole.ROLE_ADMIN);
+        boolean isOwner = comment.getAuthor().getUserId().equals(user.getUserId());
+
+        if (isAdmin || isOwner) {
             commentRepository.delete(comment);
-        }else{
+            log.info("Comment ID {} deleted by user '{}'", commentId, user.getUserName());
+        } else {
+            log.warn("User '{}' attempted unauthorized deletion of comment ID {}", user.getUserName(), commentId);
             throw new ApiException("You are not allowed to delete this comment", HttpStatus.FORBIDDEN);
         }
     }
 
     @Override
     public CommentPageResponse getAllComments(Integer page, Integer size, String sortBy, String sortOrder) {
+        log.info("Fetching all comments (page={}, size={}, sortBy={}, order={})", page, size, sortBy, sortOrder);
+
         Sort sort = "desc".equalsIgnoreCase(sortOrder)
                 ? Sort.by(sortBy).descending()
                 : Sort.by(sortBy).ascending();
@@ -136,7 +168,8 @@ public class CommentServiceImpl implements CommentService {
         resp.setTotalElements(commentPage.getTotalElements());
         resp.setTotalPages(commentPage.getTotalPages());
         resp.setLastPage(commentPage.isLast());
+
+        log.info("Fetched {} total comments", commentPage.getTotalElements());
         return resp;
     }
-
 }
