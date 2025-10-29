c2e27d7bedd67cd15baae11827e3bbb4a4e2e1f6
José Necho <jose@necho.pt>
2025-10-29 14:47:00 +0000

    Vlad

--- back/src/main/java/com/raxrot/back/BackApplication.java
+++ back/src/main/java/com/raxrot/back/BackApplication.java
@@ -2,7 +2,7 @@ package com.raxrot.back;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//vlad vlad vlad vlad
+//vlad vlad vlad vlad vlad
 @SpringBootApplication
 public class BackApplication {
 
