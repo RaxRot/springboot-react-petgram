35cab309adb73662e5fc168ddd0611a9380f7ea4
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-10-29 15:12:35 +0000

    add methods

--- back/src/main/java/com/raxrot/back/repositories/DonationRepository.java
+++ back/src/main/java/com/raxrot/back/repositories/DonationRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDateTime;
 import java.util.List;
-//test
+//testvdfe
 public interface DonationRepository extends JpaRepository<Donation, Long> {
     List<Donation> findAllByOrderByCreatedAtDesc();
 
